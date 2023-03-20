f5426bfc6107807340aa4a0181452bfbcb8dc875
pushpraj paroha <satyamparoha786@gmail.com>
2023-03-20 21:00:20 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React,{useEffect } from "react";
 import  {fetchDataFromApi}  from "./utils/api";
-
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from "./store/homeSlice"
 function App() {
- 
+const dispatch = useDispatch();
+const  { url } =useSelector((state)=>state.home);
+
  useEffect(
   ()=>{
     apitesting();
@@ -11,13 +14,13 @@ function App() {
  const apitesting = () =>{
   fetchDataFromApi("/movie/popular")
   .then((res) =>{
-      console.log(res)
+       dispatch(getApiConfiguration(res))
     }
   )
  }
   return (
     <div className="App">
-      
+      {url?.data?.total_pages}
     </div>
   );
 }
