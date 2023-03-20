6dc84831372d1179a111e9af8fdd51c89bdd69ab
pushpraj paroha <satyamparoha786@gmail.com>
2023-03-20 19:03:39 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React,{useEffect } from "react";
+import  {fetchDataFromApi}  from "./utils/api";
 
 function App() {
+ 
+ useEffect(
+  ()=>{
+    apitesting();
+  }
+ ,[])
+ const apitesting = () =>{
+  fetchDataFromApi("/movie/popular")
+  .then((res) =>{
+      console.log(res)
+    }
+  )
+ }
   return (
     <div className="App">
       
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,12 @@
-import { axios } from "axios"
+import axios from "axios";
 const BASE_URL = "https://api.themoviedb.org/3"
 
 const TMDB_TOKEN = process.env.REACT_APP_TMDB_API_TOKEN;
 const headers = {
-   Authorization: "bearer" + TMDB_TOKEN,
+   Authorization: "bearer " + TMDB_TOKEN,
 };
 
-export const fetchDateFromApi = async (url , params)=>{
+export const fetchDataFromApi = async (url , params)=>{
  try{
    const data = await axios.get(BASE_URL + url ,{
     headers: headers,
