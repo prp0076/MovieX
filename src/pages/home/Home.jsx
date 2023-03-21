e8ab5974526a71fa6d7b5ada30401dcc3c722aa5
pushpraj paroha <satyamparoha786@gmail.com>
2023-03-22 00:55:56 +0530

    ud

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./Home.scss"
 import HeroBanner from './heroBanner/HeroBanner'
+import Trending from './trending/Trending'
 const Home = () => {
   return (
     <div className='homePage'>
       <HeroBanner/>
-      <div style={{height: 1000}}></div>
+      <Trending/>
     </div>
   )
 }
