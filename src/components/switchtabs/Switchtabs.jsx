026f0d8aaaef2d56cccd17a90ae1e8e7c294cd5d
pushpraj paroha <satyamparoha786@gmail.com>
2023-03-22 01:46:28 +0530

    api call

--- src/components/switchtabs/Switchtabs.jsx
+++ src/components/switchtabs/Switchtabs.jsx
@@ -3,15 +3,25 @@ import "./Switchtabs.scss"
 const Switchtabs = ({data, onTabChange}) => {
  const[selectiveTab,setselectiveTab]=useState(0);
  const [left ,setLeft]=useState(0);
+
+ const activeTab =(tab,index)=>{
+  setLeft(index * 100);
+  setTimeout(()=>{
+   setselectiveTab(index);
+  },300)
+  onTabChange(tab,index);
+ }
   return (
     <div className='switchingTabs'>
         <div className="tabItems">
             {data.map((tab,index)=>(
-             <span key={index} className={`tabItem`}>
+             <span onClick={()=>{
+              activeTab(tab,index)
+             }} key={index} className={`tabItem ${selectiveTab === index ?"active":""}`}>
                 {tab}
              </span>
             ))}
-            <span className="movieBg" style={{left}}></span>
+            <span className="movingBg" style={{left}}></span>
         </div>
     </div>
   )
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
 import Switchtabs from '../../../components/switchtabs/Switchtabs'
+import useFetch from "../../../hooks/usefetch"
 const Trending = () => {
+    const[endpoint,setEndpoint]=useState("day");
+    const [data,loading]=useFetch(`/trending/all/${endpoint}`);
     //api call when tab chages
     const onTabChange =(tab)=>{
-
+     setEndpoint(tab === "Day"?"day":"week");
     }
+    
+    
   return (
     <div className='carouselSection'>
         <ContentWrapper>
