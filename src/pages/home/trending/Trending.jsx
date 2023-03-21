e68f8302c75c1c5d763b3bdc809c4a0fbbdd9a82
pushpraj paroha <satyamparoha786@gmail.com>
2023-03-22 01:13:42 +0530

    up

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
 
 const Trending = () => {
   return (
-    <div>Trending</div>
+    <div className='carouselSection'>
+        <ContentWrapper>
+            <span className="carouselTitle">Trending</span>
+        </ContentWrapper>
+    </div>
   )
 }
 
