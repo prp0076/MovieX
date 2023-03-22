565c998ca0c949ccb1142505e86642ccb0490b90
pushpraj paroha <satyamparoha786@gmail.com>
2023-03-22 18:29:19 +0530

    detailsbanner

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import "./Detail.scss"
+import {useParams} from "react-router-dom"
+import useFetch from "../../hooks/usefetch"
+import DetailsBanner from './detailsBanner/DetailsBanner'
 const Details = () => {
+  //api call for details
+  const {mediaType,id}=useParams();
+  const {data,loading}=useFetch(`/${mediaType}/${id}`)
   return (
-    <div>Details</div>
+    <div>
+      <DetailsBanner/>
+    </div>
   )
 }
-
+ 
 export default Details
