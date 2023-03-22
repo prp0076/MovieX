c7dd47b41e66807e6ca9a8cf68e5179fbdb5ebb1
pushpraj paroha <satyamparoha786@gmail.com>
2023-03-23 02:35:02 +0530

    up

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -8,8 +8,8 @@ const Details = () => {
   const {mediaType,id}=useParams();
   const {data,loading}=useFetch(`/${mediaType}/${id}/videos`)
   const {data:credits,loading:creditsloading}=useFetch(`/${mediaType}/${id}/credits`)
-  console.log(data?.data?.results[0])
-  console.log(credits?.credits?.crew)
+  // console.log(data?.data?.results[0])
+  // console.log(credits?.data?.crew)
 
   return (
     <div>
