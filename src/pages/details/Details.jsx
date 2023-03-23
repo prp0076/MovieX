08dc0e2bb99ae24a455cc8bdacdc6a41f1dffefc
pushpraj paroha <satyamparoha786@gmail.com>
2023-03-23 14:21:17 +0530

    cast-component-inside-details

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,18 +3,21 @@ import "./Detail.scss"
 import {useParams} from "react-router-dom"
 import useFetch from "../../hooks/useFetch"
 import DetailsBanner from './detailsBanner/DetailsBanner'
+import Cast from './cast/Cast'
+
 const Details = () => {
   
   const {mediaType,id}=useParams();
   const {data,loading}=useFetch(`/${mediaType}/${id}/videos`)
   const {data:credits,loading:creditsloading}=useFetch(`/${mediaType}/${id}/credits`)
   // console.log(data?.data?.results[0])
-  // console.log(credits?.data)
+  console.log(credits?.data)
   // console.log(data)
 
   return (
     <div>
       <DetailsBanner video={data?.data?.results[0]} crew={credits?.data?.crew}/>
+      <Cast/>
     </div>
   )
 }
---
+++ src/pages/details/cast/Cast.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import "./Cast.scss";
+
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import Img from "../../../components/lazyLoadImage/Img";
+import avatar from "../../../assets/avatar.png";
+
+const Cast = ({ data, loading }) => {
+    const { url } = useSelector((state) => state.home);
+
+    const skeleton = () => {
+        return (
+            <div className="skItem">
+                <div className="circle skeleton"></div>
+                <div className="row skeleton"></div>
+                <div className="row2 skeleton"></div>
+            </div>
+        );
+    };
+    return (
+        <div className="castSection">
+            <ContentWrapper>
+                <div className="sectionHeading">Top Cast</div>
+                {!loading ? (
+                    <div className="listItems">
+                        Cast Data....
+                    </div>
+                ) : (
+                    <div className="castSkeleton">
+                        {skeleton()}
+                        {skeleton()}
+                        {skeleton()}
+                        {skeleton()}
+                        {skeleton()}
+                        {skeleton()}
+                    </div>
+                )}
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default Cast;
+
+// CSS
