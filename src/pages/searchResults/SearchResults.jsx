14015636f64a993ecca2fbe42fcfba9f61077441
pushpraj paroha <satyamparoha786@gmail.com>
2023-03-25 00:41:17 +0530

    searchresult

--- src/pages/searchResults/SearchResults.jsx
+++ src/pages/searchResults/SearchResults.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./SearchResults.scss"
-
+import InfiniteScroll from "react-infinite-scroll-component"
+import { useParams } from 'react-router-dom'
+import { fetchDataFromApi } from '../../utils/api'
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper'
+import MovieCard from "../../components/moviecard/MovieCard"
+import Spinner from "../../components/spinner/Spinner"
 const SearchResults = () => {
   return (
-    <div>SearchResults</div>
+    <div>
+      
+    </div>
   )
 }
 
