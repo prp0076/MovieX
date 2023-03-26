c00430701f210de189cc2909b0f7596abe4c6520
pushpraj paroha <satyamparoha786@gmail.com>
2023-03-26 17:01:17 +0530

    explore

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Select from "react-select";
 
-import "./style.scss";
+import "./Explore.scss";
 
 import useFetch from "../../hooks/usefetch";
 import { fetchDataFromApi } from "../../utils/api";
